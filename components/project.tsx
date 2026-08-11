cab793be28a4c21a01ac80ccfdc32978306c8057
Nguyễn Thái Long <ntlong058@gmail.com>
2026-08-11 16:00:31 +0700

    feat: update project descriptions and enhance layout in Project component

--- components/project.tsx
+++ components/project.tsx
@@ -3,7 +3,7 @@
 export default function Project() {
     const projectItem = [
         {   title:"Tortoise-Nest-Online-V2", 
-            description:"Tortoise-Nest-Online-V2 là một nền tảng học trực tuyến (Learning Management System) được phát triển bằng NestJS cho backend và Next.js cho frontend. Dự án bao gồm các tính năng quản lý khóa học, bài học, học viên, giảng viên, giao dịch thanh toán và báo cáo tài chính.",
+            description:"Tortoise-Nest-Online-V2 is an online learning platform (Learning Management System) developed using NestJS for the backend and Next.js for the frontend. The project includes features for managing courses, lessons, students, payment transactions, and financial reports.",
             role:"Frontend Developer",
             tags:["Next.js", "TypeScript", "TailwindCSS"],
             spring:{
@@ -12,7 +12,7 @@ export default function Project() {
             }
         },
         {   title:"Hypee_topiks_application", 
-            description:"B", 
+            description:"Hypee_topiks_application is a mobile application developed using Flutter and Dart. The app allows users to browse and purchase various topics and courses.",
             role:"Frontend Developer", 
             tags:["Flutter", "Dart"],
             spring:{
@@ -33,48 +33,98 @@ export default function Project() {
     };
     
   return (
-    <div className='w-full mx-auto'>
-        <div className='mb-6'>  
-            <p className='font-mono text-xs text-blue-700 tracking-widest uppercase mb-2'>03. Career</p>
+    <section  className='max-w-7xl mx-auto py-20 px-6'>
+        <div className='mb-6'>
+            <p className='font-mono text-xs text-blue-700 tracking-widest uppercase mb-2'>02. Career</p>
             <div className='flex items-end justify-between flex-wrap gap-4'>
                 <h2 className='text-5xl font-bold'>Projects</h2>
             </div>
-            <div className='mt-4 h-px w-50 bg-linear-to-r from-blue-500 to-transparent'></div>
+            <div className='mt-5 h-px w-50 bg-linear-to-r from-blue-500 to-transparent'></div>
         </div>
 
         <div className='relative'>
             <div className='absolute left-1/2 top-0 h-full w-px -translate-x-1/2 bg-blue-300'></div>
             {
-                projectItem.map((project, index) => (
-                    <div key={project.title} className='grid grid-cols-2 gap-20 py-20 relative'>
-                        <div className='absolute left-1/2 top-10 -translate-x-1/2 w-12 h-12 rounded-full bg-blue-500 text-white flex justify-center items-center border-4 border-white'>
-                            {(index + 1).toString().padStart(2, '0')}
-                        </div>
-                        <div>
-                            <h2>{project.title}</h2>
-                            <p>{project.role}</p>
-                            <p>
-                                {formatMonth(project.spring.start)}
-                                -
-                                {formatMonth(project.spring.end)}
-                            </p>
-                            <div className='text-sm text-gray-500'>
-                                {project.tags.map((tag, index) => (
-                                    <span key={index} className='bg-blue-100 text-blue-800 px-2 py-1 rounded-md mr-2'>
-                                        {tag}
-                                    </span>
-                                ))
-                                }
+                projectItem.map((project, index) => {
+                    const isLeft = index % 2 === 0;
+                    return (
+                        <div key={project.title} className="relative grid grid-cols-2 gap-20 py-20">
+                            <div className="absolute top-24 left-1/2 -translate-x-1/2 w-14 h-14 rounded-full bg-blue-500 text-white flex items-center justify-center font-bold border-4 border-white shadow-lg">
+                                {(index + 1).toString().padStart(2, '0')}
                             </div>
+                            
+                            {isLeft ? (
+                                <>
+                                    <div className="relative pr-10 flex flex-col justify-center">
+                                        <span className="absolute text-[180px] font-black text-blue-50 -z-10 right-0 top-0">
+                                            {(index + 1).toString().padStart(2, '0')}
+                                        </span>
+                                        <span className="text-xs uppercase tracking-[30x] text-blue-500 rounded-full border border-blue-200 bg-blue-50 px-4 py-1 w-fit">
+                                            Project
+                                        </span>
+                                        <h2 className="text-3xl font-bold mt-3">
+                                            {project.role}
+                                        </h2>
+                                        <h3 className="text-lg font-semibold mt-2 text-blue-500">
+                                            {project.title}
+                                        </h3>
+                                        <div className="mt-4 inline-flex w-fit rounded-full border border-blue-200 bg-blue-50 px-4 py-2 text-sm text-blue-600">
+                                            {formatMonth(project.spring.start)} -{" "}
+                                            {formatMonth(project.spring.end)}
+                                        </div>
+                                    </div>
+                                    <div className="bg-white rounded-xl border border-blue-200 p-8 shadow-xl hover:-translate-y-1 transition-all">
+                                        <p className="leading-8 text-gray-800">
+                                            {project.description}
+                                        </p>
+                                        <div className="mt-8 flex flex-wrap gap-3">
+                                            {project.tags.map((tag) =>(
+                                                <span key={tag} className="px-3 py-1 rounded-full border border-blue-200 bg-blue-50 text-blue-600 text-sm">
+                                                    {tag}
+                                                </span>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div className="bg-white rounded-3xl border border-blue-200 p-8 shadow-xl hover:-translate-y-1 transition-all"> 
+                                        <p className="leading-8 text-gray-800">
+                                            {project.description}
+                                        </p>
+                                         <div className="flex flex-wrap gap-3 mt-8">
+                                            {project.tags.map((tag) => (
+                                            <span key={tag} className=" px-3 py-1 rounded-full border border-blue-200 bg-blue-50 text-blue-600 text-sm">
+                                                {tag}
+                                            </span>
+                                            ))}
+                                        </div>
+                                    </div>
+                                    <div className="relative pl-10 flex flex-col justify-center">
+                                        <span className="absolute text-[180px] font-black text-blue-50 -z-10 right-0 top-0">
+                                            {(index + 1).toString().padStart(2, '0')}
+                                        </span>
+                                        <span className="text-xs uppercase tracking-[30x] text-blue-500 rounded-full border border-blue-200 bg-blue-50 px-4 py-1 w-fit">
+                                            Project
+                                        </span>
+                                        <h2 className="text-3xl font-bold mt-3">
+                                            {project.role}
+                                        </h2>
+                                        <h3 className="text-lg font-semibold mt-2 text-blue-500">
+                                            {project.title}
+                                        </h3>
+                                        <div className="mt-6 inline-flex w-fit rounded-full border border-blue-200 bg-blue-50 px-4 py-2 text-sm text-blue-600">
+                                            {formatMonth(project.spring.start)} -{" "}
+                                            {formatMonth(project.spring.end)}
+                                        </div>
+                                    </div>
+                                </>
+                            )}
                         </div>
-                        
-                        <div>
-                            {project.description}
-                        </div>
-                    </div>    
-                ))
+                    )
+                })
             }
         </div>
-    </div>
+    </section>
   )
 }
